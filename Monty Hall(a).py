4c964be00e3172ac56872ae670862182d6de3622
Anthony Givans <58392771+GivyBoy@users.noreply.github.com>
2020-09-04 10:59:15 -0500

    Add files via upload

---
+++ Monty Hall(a).py
@@ -0,0 +1,45 @@
+from random import randint
+
+print('Choose either Door 1, Door 2, or Door 3 to see if you are a winner.')
+
+a = int(input('Enter your door choice here: '))
+
+if 1 <= a <= 3:
+    print('Great Choice')
+else:
+    print('Choose a number from 1 to 3')
+
+count = 0
+for i in range(10000):
+    prize = randint(1, 3)
+    if prize == 1:
+        s = randint(2, 3)
+        doors = prize, s
+        if a == s:
+            a = prize
+        else:
+            a = s
+    elif prize == 2:
+        q = randint(1, 3)
+        doors = q, prize
+        if a == q:
+            a = prize
+        else:
+            a = q
+    else:
+        v = randint(1, 2)
+        doors = v, prize
+        if a == v:
+            a = prize
+        else:
+            a = v
+    if a == prize:
+        count = count + 1
+
+percent = (count / 10000)*100
+
+print(f'Congrats! You won percent {percent}% of the time.')
+
+
+
+
